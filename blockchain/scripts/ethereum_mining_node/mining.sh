61b077d92fcbebdccf09ddf330934a3eb75a1531
Yushi Quist <yushi.quist@gmail.com>
2019-03-31 06:15:20 +0900

    upgrade web3j to 4.2.0

--- blockchain/scripts/ethereum_mining_node/mining.sh
+++ blockchain/scripts/ethereum_mining_node/mining.sh
@@ -1,4 +1,4 @@
 HOST_IP=`hostname -i`
 BOOT_NODE_IP=$1
 ETHERBASE=$2
-geth --networkid 9239459 --datadir=data --port 30303 --syncmode "full" --rpc --rpcaddr $HOST_IP --rpcport 8545 --rpccorsdomain "*" --rpcapi "admin,eth,net,web3,personal" --ipcdisable --mine --miner.threads=1 --miner.gasprice "0" --miner.gastarget "0" --miner.etherbase=$ETHERBASE --lightserv 50 --bootnodes=enode://4ac77627e4236535c8778b66b0e1c440b190642eb7bd01a18c49f6a0893ebc8009f3d1712d7a5f61322c26be2f888e3baf5c713a394f7b6d7bfd67fa2f6e1dfd@[$BOOT_NODE_IP]:30301 >>mining.log 2>&1 &
+geth --networkid 9239459 --datadir=data --port 30303 --syncmode "full" --rpc --rpcaddr $HOST_IP --rpcport 8545 --rpccorsdomain "*" --rpcapi "admin,eth,net,web3,personal" --ipcdisable --mine --miner.threads=1 --miner.gasprice "0" --miner.etherbase=$ETHERBASE --lightserv 50 --bootnodes=enode://4ac77627e4236535c8778b66b0e1c440b190642eb7bd01a18c49f6a0893ebc8009f3d1712d7a5f61322c26be2f888e3baf5c713a394f7b6d7bfd67fa2f6e1dfd@[$BOOT_NODE_IP]:30301 >>mining.log 2>&1 &
