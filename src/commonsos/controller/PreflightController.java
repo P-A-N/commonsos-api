6d468baeddcc2d1180918957bee7a96a129a04fa
Yushi Quist <yushi.quist@gmail.com>
2019-07-17 23:34:49 +0900

    get preflight url from configuration

--- src/commonsos/controller/PreflightController.java
+++ src/commonsos/controller/PreflightController.java
@@ -1,5 +1,8 @@
 package commonsos.controller;
 
+import javax.inject.Inject;
+
+import commonsos.Configuration;
 import commonsos.annotation.ReadOnly;
 import spark.Request;
 import spark.Response;
@@ -8,9 +11,11 @@ import spark.Route;
 @ReadOnly
 public class PreflightController implements Route {
 
+  @Inject Configuration config;
+
   @Override
   public Object handle(Request request, Response response) {
-    response.header("Access-Control-Allow-Origin", "http://commonspeople.localhost:8888");
+    response.header("Access-Control-Allow-Origin", config.preflightUrl());
     response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     response.header("Access-Control-Allow-Headers", "Origin, Content-Type");
     response.header("Access-Control-Max-Age", "3600");
