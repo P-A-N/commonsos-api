b147fc3450d77299e0b53b48e5a6af08c8b4b418
Yushi Quist <yushi.quist@gmail.com>
2019-07-18 22:43:58 +0900

    modified request headers

--- src/commonsos/filter/AddHeaderFilter.java
+++ src/commonsos/filter/AddHeaderFilter.java
@@ -1,14 +1,18 @@
 package commonsos.filter;
 
+import javax.inject.Inject;
+
+import commonsos.Configuration;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
 
 public class AddHeaderFilter implements Filter {
 
+  @Inject Configuration config;
+
   @Override
   public void handle(Request request, Response response) throws Exception {
     response.type("application/json");
-    response.header("Access-Control-Allow-Origin", "http://commonspeople.localhost:8888");
   }
 }
